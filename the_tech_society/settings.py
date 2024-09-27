b0d0937f977867a88deeb533ea84e0962f2a9c7a
Vineet Kumar <itsvineetkr@gmail.com>
2024-09-27 21:46:15 +0530

    Update settings.py

--- the_tech_society/settings.py
+++ the_tech_society/settings.py
@@ -150,4 +150,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'vineetkumarvks.8866@gmail.com'
-EMAIL_HOST_PASSWORD = 'gilflsttjjeiwfqb'
+EMAIL_HOST_PASSWORD = ''
